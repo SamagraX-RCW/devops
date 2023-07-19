2c323d784c90f23c1102f8f75674c412a23b6a40
Yuvraj Chhetri <yuvichh01@gmail.com>
2023-07-20 02:36:49 +0530

    added docker-compose installation

--- setup.sh
+++ setup.sh
@@ -3,28 +3,35 @@
 # Update the dependencies
 sudo apt-get update
 
+# Install dependencies
+sudo apt-get install ca-certificates curl gnupg
+
 # Check if Docker is installed
 if ! command -v docker &> /dev/null; then
     echo "Docker is not installed. Installing Docker..."
     
     # Install Docker dependencies
-    sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-    
-    # Add Docker's official GPG key
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-    
+    sudo install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
     # Add Docker repository
-    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    
-    # Update the package index (again)
+    echo \
+    "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
     sudo apt-get update
-    
+
     # Install Docker
-    sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
     # Add current user to docker group
     sudo groupadd docker
     sudo usermod -aG docker $USER
+
+    # Install Docker Compose
+    sudo apt-get install docker-compose-plugin
     
     echo "Docker installed successfully!"
 else
