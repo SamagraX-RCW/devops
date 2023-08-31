bcfe42aa1fa84b5e9b82a22d8bad948decf0357a
Yuvraj Chhetri <yuvichh01@gmail.com>
2023-08-31 23:55:06 +0530

    updated scripts

--- scripts/jenkins_init.sh
+++ scripts/jenkins_init.sh
@@ -68,11 +68,6 @@ curl -X POST -u "admin:$password" $url/setupWizard/createAdminUser \
      --cookie $cookie_jar \
      --data-raw "username=$username&password1=$new_password&password2=$new_password&fullname=$fullname&email=$email&Jenkins-Crumb=$only_crumb&json=%7B%22username%22%3A%20%22$username%22%2C%20%22password1%22%3A%20%22$new_password%22%2C%20%22%24redact%22%3A%20%5B%22password1%22%2C%20%22password2%22%5D%2C%20%22password2%22%3A%20%22$new_password%22%2C%20%22fullname%22%3A%20%22$fullname%22%2C%20%22email%22%3A%20%22$email%22%2C%20%22Jenkins-Crumb%22%3A%20%22$only_crumb%22%7D&core%3Aapply=&Submit=Save&json=%7B%22username%22%3A%20%22$username%22%2C%20%22password1%22%3A%20%22$new_password%22%2C%20%22%24redact%22%3A%20%5B%22password1%22%2C%20%22password2%22%5D%2C%20%22password2%22%3A%20%22$new_password%22%2C%20%22fullname%22%3A%20%22$fullname%22%2C%20%22email%22%3A%20%22$email%22%2C%20%22Jenkins-Crumb%22%3A%20%22$only_crumb%22%7D"
 
-cookie_jar="$(mktemp)"
-full_crumb=$(curl -u "$user:$password" --cookie-jar "$cookie_jar" $url/crumbIssuer/api/xml?xpath=concat\(//crumbRequestField,%22:%22,//crumb\))
-arr_crumb=(${full_crumb//:/ })
-only_crumb=$(echo ${arr_crumb[1]})
-
 # MAKE THE REQUEST TO DOWNLOAD AND INSTALL REQUIRED MODULES
 curl -X POST -u "$user:$password" $url/pluginManager/installPlugins \
 -H 'Connection: keep-alive' \
@@ -84,9 +79,6 @@ curl -X POST -u "$user:$password" $url/pluginManager/installPlugins \
 --cookie $cookie_jar \
 --data-raw "{'dynamicLoad':true,'plugins':['cloudbees-folder','antisamy-markup-formatter','build-timeout','credentials-binding','timestamper','ws-cleanup','ant','gradle','workflow-aggregator','github-branch-source','pipeline-github-lib','pipeline-stage-view','git','ssh-slaves','matrix-auth','pam-auth','ldap','email-ext','mailer'],'Jenkins-Crumb':'$only_crumb'}"
 
-sudo ln -s ./jobs/RCW /var/lib/jenkins/jobs/RCW
-
-chown -R jenkins:jenkins /var/lib/jenkins/jobs
-chown -R jenkins:jenkins /home/devops/jobs/
+sudo cp ~/devops/jobs/RCW /var/lib/jenkins/jobs/RCW
 
 sudo systemctl restart jenkins
